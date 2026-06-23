c3a56e9d56100a5155a3ce453667a7e4c8f94b24
HyeJin <155633287+Hyejinijini@users.noreply.github.com>
2026-06-23 21:25:06 +0900

    260616: Data Type
    자료형

---
+++ data_type.py
@@ -0,0 +1,22 @@
+# 숫자형 자료형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+
+# 문자열 자료형
+print('풍선')
+print("나비")
+print("ㅎ"*9) # 'ㅎ' 문자가 9번 출력됨
+print('ㅎㅇㅎㅇ')
+
+# 참/거짓 논리형 자료형
+print(5 > 10) # False
+print(5 < 10) # True
+print(True)
+print(False)
+print(not True) # False
+print(not (5 > 10)) # True
