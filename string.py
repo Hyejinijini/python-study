88b09664545934a26b97b7f2dfea825f98c8cd07
HyeJin <155633287+Hyejinijini@users.noreply.github.com>
2026-06-23 21:30:08 +0900

    260616: String
    문자열

---
+++ string.py
@@ -0,0 +1,16 @@
+# 문자열
+# 작은따옴표('')
+# 큰따옴표 ("")
+# 큰따옴표 3개("""")
+
+sentence = '나는 소년입니다'
+print(sentence) # 나는 소년입니다
+
+sentence2 = "파이썬"
+print(sentence2) # 파이썬
+
+sentence3 = """
+나는 소년이고,
+파이썬
+"""
+print(sentence3) # 나는 소년이고, 파이썬
