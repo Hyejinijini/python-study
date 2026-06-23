752eb58f4ad91ee961dea526813e11cbe6e40881
HyeJin <155633287+Hyejinijini@users.noreply.github.com>
2026-06-23 21:35:17 +0900

    260622: Random Function
    랜덤 함수(난수)

---
+++ random_function.py
@@ -0,0 +1,33 @@
+# 랜덤 함수 = 난수(무작위)
+
+from random import * # random 라이브러리 모든 것을 이용하겠다는 의미
+
+# print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+# print(random() * 10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+# print(int(random() * 10)) # 소수점 없애기 = int(정수), 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+
+# 마지막 값 포함 X
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+
+# 마지막 값 포함 O
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
