34937ad615b7703ca2b335228752a00e2b4b141b
HyeJin <155633287+Hyejinijini@users.noreply.github.com>
2026-08-11 15:52:27 +0900

    260811: List
    리스트

---
+++ 5-1_list.py
@@ -0,0 +1,54 @@
+#  리스트 []
+
+# 지하철 칸별로 10명, 20명, 30명
+subway1 = 10
+subway2 = 20
+subway3 = 30
+
+subway = [10, 20, 30]
+print(subway) # [10, 20, 30]
+
+subway = ["유재석", "조세호", "박명수"]
+print(subway) # ["유재석", "조세호", "박명수"]
+
+# 조세호씨가 몇 번째 칸에 타고 있는가?
+print(subway.index("조세호")) # 1
+
+# 하하씨가 다음 정류장에서 다음 칸에 탐
+print(subway.append("하하")) # append : 리스트 맨 뒤에 추가
+print(subway) # ["유재석", "조세호", "박명수", "하하"]
+
+# 정형돈씨를 유재석 / 조세호 사이에
+subway.insert(1, "정형돈") # 추가할 위치, 추가할 값
+print(subway) # ["유재석", "정형돈"", "조세호", "박명수", "하하"]
+
+# 지하철에 있는 사람을 한 명씩 뒤에서 꺼냄
+print(subway.pop()) # pop : 맨 뒤 삭제
+print(subway) # ["유재석", "정형돈"", "조세호", "박명수"]
+
+# 같은 이름의 사람이 몇 명 있는지 확인하기
+subway.append("유재석")
+print(subway) # ["유재석", "정형돈"", "조세호", "박명수", "유재석"]
+print(subway.count("유재석")) # 2
+
+# 정렬
+num_list = [5, 2, 4, 3, 1]
+num_list.sort() # 오름차순 정렬
+print(num_list) # [1, 2, 3, 4, 5]
+
+# 순서 뒤집기
+num_list.reverse()
+print(num_list) # [5 ,4, 3, 2, 1]
+
+# 모두 지우기
+num_list.clear()
+print(num_list) # []
+
+# 자료형에 구애받지 않고 다양한 자료형 함꼐 사용
+num_list [5, 2, 4, 3, 1]
+mix_list = ['조세호', 20, True]
+print(mix_list) # ['조세호', 20, True]
+
+# 리스트 확장
+num_list.extend(mix_list) # 하나의 리스트로 합치기
+print(num_list) # [5, 2, 4, 3, 1, '조세호', 20, True]
